ecdc55ef46744a7504ee93e749574aa6684f6af8
Ratnesh-Dhan <ratneshdhan@gmail.com>
2026-09-23 12:52:11 +0530

    typo error fix

--- BinaryInferenceWithClass/main.py
+++ BinaryInferenceWithClass/main.py
@@ -66,7 +66,7 @@ unetModel.eval()
 
 # Load DeeplabV3 3 class segmentation model (Only to determine class of the corrosion not overlay)
 deepLabModel = build_deeplabv3_model().to(DEVICE)
-deepLabmodelCheckpoint = torch.load("/mnt/z/codes/rootML/codex_corrosion_segmentation/outputs/checkpoints/best/pth", map_location=DEVICE)
+deepLabmodelCheckpoint = torch.load("/mnt/z/codes/rootML/codex_corrosion_segmentation/outputs/checkpoints/best.pth", map_location=DEVICE)
 deepLabModel.load_state_dict(deepLabmodelCheckpoint["model_state_dict"])
 deepLabModel.eval()
 
